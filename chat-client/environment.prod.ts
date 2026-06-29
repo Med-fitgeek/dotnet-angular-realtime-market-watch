fd151ea3c41858f589884a10dee1d236dd200cef
Ndongo Medoune NDIAYE <117470353+Med-fitgeek@users.noreply.github.com>
2026-06-29 13:55:20 +0200

    Delete chat-client directory

--- chat-client/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://hellofriends-backend-cubefpeeh0eehagg.westeurope-01.azurewebsites.net/', 
-  signalRHubUrl: 'https://hellofriends-backend-cubefpeeh0eehagg.westeurope-01.azurewebsites.net//chatHub'
-};
--- chat-client/environment.ts
+++
@@ -1,6 +0,0 @@
-// environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5099',
-  signalRHubUrl: 'http://localhost:5099/chatHub'
-};
