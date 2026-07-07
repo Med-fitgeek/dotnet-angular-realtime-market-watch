ae45b90bd1e19fa6eba12c8d3f2df3bb2c5ae901
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-07-07 21:40:37 +0200

    fix(frontend) : Added new routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,30 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent) },
-  { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' } // ← DERNIER
+  {
+    path: '',
+    loadComponent: () =>
+      import('./app.component').then(m => m.AppComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'home',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
-
